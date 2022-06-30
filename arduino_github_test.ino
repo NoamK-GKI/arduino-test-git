854f2eea74c25678005bf6fee3e7f349021b8e69
Noam <noam@gkigroup.com>
2022-06-30 11:49:40 +0300

    led off

--- arduino_github_test.ino
+++ arduino_github_test.ino
@@ -8,11 +8,11 @@ arduino test code  github
 
 void setup() {
   // put your setup code here, to run once:
-
+pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-
+digitalWrite(LED_BUILTIN,LOW);
 }
